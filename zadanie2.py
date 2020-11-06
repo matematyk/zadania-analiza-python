be0c2ce9332414ba4bd7b2a4b188dd4d55a9c56e
Marcin Wierzbiński <marcin119a@gmail.com>
2020-11-06 23:03:54 +0100

    zad2

--- zadanie2.py
+++ zadanie2.py
@@ -6,17 +6,26 @@ The program should ask the user for the total number of iterations and the step 
 the current approximation after each step number of iterations and then the final value.
 """
 
-def monte_carlo(points_in_square=0):
+def monte_carlo(points_in_square=10 ** 5, step_of_printing = 10):
     points_in_circle = 0
 
     
-    for i in range(1, points_in_square):
+    for i  in range(1, points_in_square):
         x, y = rd.uniform(-1,1), rd.uniform(-1,1)
 
         if(x** 2 + y ** 2 <= 1):
-            points_in_circle += 1
-
-    PI = 4 * points_in_circle / points_in_square
+                points_in_circle += 1
+        
+        PI = 4 * points_in_circle / points_in_square
+        if i % step_of_printing == 0:  
+            print(PI)
+        
     return PI 
 
-print(monte_carlo(points_in_square=10**5))
+
+
+if __name__ == "__main__":
+    iteraction = int(input("Please define number of iteration"))
+    step = int(input("Please define step of printing value"))
+    print(monte_carlo(points_in_square=iteraction, step_of_printing=step))
+
